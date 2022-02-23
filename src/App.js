a36a6ce817b56278d8cb42d9631a21ca454c7b70
Long <longish222@gmail.com>
2022-02-22 23:50:22 -0800

    really good progress. navbar works with mui and react Link. Theme template created and ready to be changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+// import About from './components/About/About';
+import ProductList from './components/Products/ProductList';
+import Socials from './components/Socials/Socials';
+
+import { ThemeProvider } from '@mui/material'
+import theme from './theme'
+
+
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      Main page
-    </div>
+    <ThemeProvider theme={ theme }>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={ <Home /> } />
+          <Route path='/about' exact element={ <Home />} />
+          <Route path='/products' element={ <ProductList />} />
+          <Route path='/socials' element={ <Socials />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+
+
+
+    // <div className="App">
+    //   <Navbar />
+    //   Main page
+    // </div>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,25 +3,44 @@
 // Navbar -> Home (Logo) || About || Products || Socials || Login || Sidebar
 // - Later - Shopping cart stuff
 // - Later - Login
-import React from 'react'
-import './Navbar.css'
-import logo from '../../images/logo193.png'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from "react-router-dom";
+
+import logo from '../../images/logo.png'
 
 const Navbar = () => {
-  return (
-    <div>Navbar
-        <nav>
-            <img src={logo} alt="logo.png" className="nav-logo"/>
-            <ul>
-                <li className="nav-right-menu">About</li>
-                <li className="nav-right-menu">Products</li>
-                <li className="nav-right-menu">Socials</li>
-                <li className="nav-right-menu">Login</li>
-            </ul>
-        </nav>
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+            <Toolbar>
+                {/* UNCOMMENT FOR SIDE BAR */}
+            {/* <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <MenuIcon />
+            </IconButton> */}
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link to="/"><img src={ logo } alt="Impatiens Logo"/></Link>
+            </Typography>
+            <Button color="inherit"><Link to="about">About</Link></Button>
+            <Button color="inherit"><Link to="products">Products</Link></Button>
+            <Button color="inherit"><Link to="socials">Socials</Link></Button>
 
-    </div>
-  )
+            </Toolbar>
+        </AppBar>
+        </Box>
+    );
 }
 
 export default Navbar
