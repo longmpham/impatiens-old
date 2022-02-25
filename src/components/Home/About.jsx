9951f47f93db374a315d0c2c71babe09ab43e7e1
Long <longish222@gmail.com>
2022-02-24 16:55:49 -0800

    added about page with papers as templates

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -3,30 +3,40 @@ import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 
 import './About.css'
+import { Typography } from '@mui/material'
 
+import {FaCanadianMapleLeaf, FaThumbsUp } from 'react-icons/fa'
+import { GiSpotedFlower } from 'react-icons/gi'
 
 const About = () => {
   return (
     <Grid container justifyContent="center" mt={3} spacing={2}>
-      <Grid container justifyContent="center" spacing={2}>
+      <Grid item xs={12} mb={3}>
+        {/* CONSIDER DOING A SMALLER COMPONENT? */}
+        <Paper className="about-block" elevation={0}><Typography variant="h2">Impatient about Impatiens?</Typography></Paper>
+      </Grid>
+      <Grid container justifyContent="center" spacing={2} ml={2} mr={2}>
         <Grid item xs={12} md={4}>
           {/* CONSIDER DOING A SMALLER COMPONENT? */}
-          <Paper className="about-block" elevation={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Paper>
+          <Paper className="about-block" elevation={10}><Typography variant="h3"><FaCanadianMapleLeaf color='red'/>Canadian Made</Typography></Paper>
         </Grid>
         <Grid item xs={12} md={4}>
           {/* CONSIDER DOING A SMALLER COMPONENT? */}
-          <Paper className="about-block" elevation={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Paper>
+          <Paper className="about-block" elevation={10}><Typography variant="h3"><GiSpotedFlower color="blue"/>Organic Materials</Typography></Paper>
         </Grid>
         <Grid item xs={12} md={4}>
           {/* CONSIDER DOING A SMALLER COMPONENT? */}
-          <Paper className="about-block" elevation={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Paper>
+          <Paper className="about-block" elevation={10}><Typography variant="h3"><FaThumbsUp color="#ffcc33" />Hand Crafted</Typography></Paper>
         </Grid>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} ml={2} mr={2}>
         {/* CONSIDER DOING A SMALLER COMPONENT? */}
         <Paper className="about-block" elevation={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Paper>
       </Grid>
-
+      <Grid item xs={12} ml={2} mr={2}>
+        {/* CONSIDER DOING A SMALLER COMPONENT? */}
+        <Paper className="about-block" elevation={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Paper>
+      </Grid>
     </Grid>
   )
 }
