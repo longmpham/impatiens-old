72d763161f38a71e976b3a62e2f9148fb0587f1c
Long <longish222@gmail.com>
2022-02-27 16:08:08 -0800

    fixed home carousel to pull api from jsonplaceholder

--- src/components/Home/Carousel.jsx
+++ src/components/Home/Carousel.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Slider from "react-slick";
 
 
-import nailList from '../../data.js'
+// import nailList from '../../data.js'
 
 const Carousel = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [nailList, setNailList] = useState([]);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+  
+            // 1. loading screen
+            // 2. get promise (await for data)
+            // 3. setdata and/or log
+            // 4. set loading false
+            // 5. call function
+  
+            // loading...
+            setLoading(true);
+  
+            const result = await axios.get("http://jsonplaceholder.typicode.com/albums/1/photos", {
+                params: {
+                    _limit: 10 // limit number of objects found (finds first 10)
+                }
+            });
+  
+            console.log(result.data);
+            setNailList(result.data);
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
 
     const sliderSettings = {
         dots: true,
@@ -29,8 +59,8 @@ const Carousel = () => {
             <Slider {...sliderSettings}>
                 {nailList.map((nail) => {
                     return (
-                        <div key={nail._id}>
-                            <img width="100%" src={nail.image[0]} alt={nail.alt}/>
+                        <div key={nail.id}>
+                            <img width="100%" src={nail.url} alt={nail.thumbnailUrl}/>
                         </div>
                     )
                 })}
